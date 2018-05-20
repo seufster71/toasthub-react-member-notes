e3a7624d65245fe8c96f5d23a5372fda692fb9dc
seufster <seufster@gmail.com>
2018-05-20 07:22:30 -0400

    init

---
+++ notes-container.js
@@ -0,0 +1,46 @@
+/*
+* Author Edward Seufert
+*/
+'use-strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as appPrefActions from '../../core/common/apppref-actions';
+import fuLogger from '../../core/common/fu-logger';
+import NotesView from '../../memberView/notes/notes-view';
+
+class NotesContainer extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	componentDidMount() {
+		//this.props.actions.initMember();
+	}
+
+  render() {
+			fuLogger.log({level:'TRACE',loc:'NotesContainer::render',msg:"Hi there"});
+      return (
+				<NotesView/>
+			);
+  }
+}
+
+NotesContainer.propTypes = {
+	appPrefs: PropTypes.object,
+	lang: PropTypes.string,
+	appGlobal: PropTypes.object,
+	actions: PropTypes.object
+};
+
+function mapStateToProps(state, ownProps) {
+  return {lang:state.lang, appPrefs:state.appPrefs};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions:bindActionCreators(appPrefActions,dispatch) };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(NotesContainer);
